83ea18ea08feb835833ea66d2eb5aa2e3af87b5f
httpe <httpe@live.jp>
2021-04-25 20:52:54 -0400

    Add unlink sys call

--- newlib/libc/sys/simpleos/syscalls.c
+++ newlib/libc/sys/simpleos/syscalls.c
@@ -28,6 +28,7 @@ static inline _syscall0(SYS_GET_PID, int, sys_get_pid);
 static inline _syscall3(SYS_SEEK, int, sys_seek, int, fd, int, offset, int, whence)
 static inline _syscall1(SYS_CURR_DATE_TIME, int, sys_curr_date_time, date_time*, dt)
 static inline _syscall1(SYS_WAIT, int, sys_wait, int*, wait_status)
+static inline _syscall1(SYS_UNLINK, int, sys_unlink, char*, path)
 
 // Exit a program without cleaning up files. 
 // If your system doesn’t provide this, it is best to avoid linking with subroutines that require it (exit, system).
@@ -153,8 +154,7 @@ clock_t times(struct tms *buf) {
 
 // Remove a file’s directory entry
 int unlink(char *name) {
-  errno = ENOENT;
-  return -1; 
+  return sys_unlink(name);
 }
 
 // Wait for a child process
