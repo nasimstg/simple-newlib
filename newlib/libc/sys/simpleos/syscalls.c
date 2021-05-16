321ba3686bf22bb9bb427ed8ca00eb20339a0810
httpe <httpe@live.jp>
2021-05-16 00:29:04 -0400

    Set error no when error

--- newlib/libc/sys/simpleos/syscalls.c
+++ newlib/libc/sys/simpleos/syscalls.c
@@ -15,7 +15,7 @@ extern int errno;
 #include <syscall.h>
 #include <kernel/stat.h>
 
-static inline _syscall1(SYS_SBRK, void*, sys_sbrk, int, size_delta)
+static inline _syscall1(SYS_SBRK, int, sys_sbrk, int, size_delta)
 static inline _syscall3(SYS_READ, int, sys_read, int, fd, void*, buf, uint, size)
 static inline _syscall3(SYS_WRITE, int, sys_write, int, fd, const void*, buf, uint, size)
 static inline _syscall2(SYS_OPEN, int, sys_open, const char*, path, int, flags)
@@ -37,6 +37,16 @@ static inline _syscall2(SYS_GETCWD, int, sys_getcwd, char *, buf, size_t, buf_si
 static inline _syscall2(SYS_TRUNCATE_FD, int, sys_truncate_fd, int, fd, uint, size)
 static inline _syscall2(SYS_TRUNCATE_PATH, int, sys_truncate_path, const char*, path, uint, size)
 
+static int set_errno(int res)
+{
+  if(res < 0) {
+    errno = -res;
+    return -1;
+  } else {
+    return res;
+  }
+}
+
 static void fs_stat2stat(fs_stat* fs_st, struct stat* st)
 {
   memset(st, 0, sizeof(*st));
@@ -55,7 +65,6 @@ static void fs_stat2stat(fs_stat* fs_st, struct stat* st)
   };
 }
 
-
 // Exit a program without cleaning up files. 
 // If your system doesn’t provide this, it is best to avoid linking with subroutines that require it (exit, system).
 void _exit(int return_code) {
@@ -64,8 +73,8 @@ void _exit(int return_code) {
 }
 
 // Close a file
-int close(int file) {
-  return sys_close(file);
+int close(int fd) {
+  return set_errno(sys_close(fd));
 }
 
 // A pointer to a list of environment variables and their values. 
@@ -75,24 +84,24 @@ char **environ = __env;
 
 // Transfer control to a new process
 int execve (const char *path, char * const argv[], char * const envp[]) {
-  return sys_exec(path, argv);
+  return set_errno(sys_exec(path, argv));
 }
 
 // Create a new process
 int fork(void) {
-  return sys_fork();
+  return set_errno(sys_fork());
 }
 
 // Status of an open file
 // For consistency with other minimal implementations in these examples, all files are regarded as character special devices.
-int fstat(int file, struct stat *st) {
+int fstat(int fd, struct stat *st) {
   fs_stat fs_st = {0};
-  int res = sys_getattr_fd(file, &fs_st);
+  int res = sys_getattr_fd(fd, &fs_st);
   if(res < 0) {
-    return res;
+    return set_errno(res);
   }
   fs_stat2stat(&fs_st, st);
-  return res;
+  return set_errno(res);
 }
 
 // Process-ID; this is sometimes used to generate strings unlikely to conflict with other processes.
@@ -102,13 +111,14 @@ int getpid(void) {
 
 // Query whether output stream is a terminal
 // For consistency with the other minimal implementations, which only support output to stdout
-int isatty(int file) {
+int isatty(int fd) {
   struct stat st = {0};
-  int res = fstat(file, &st);
+  int res = fstat(fd, &st);
   if(res < 0) {
-    return st.st_mode == S_IFCHR;
-  } else {
+    set_errno(res);
     return 0;
+  } else {
+    return st.st_mode == S_IFCHR;
   }
 }
 
@@ -120,32 +130,38 @@ int kill(int pid, int sig) {
 
 // Establish a new name for an existing file
 int link(const char *old, const char *new) {
-  return sys_link(old, new);
+  return set_errno(sys_link(old, new));
 }
 
 int _rename(const char* old, const char* new)
 {
-  return sys_rename(old, new, 0);
+  return set_errno(sys_rename(old, new, 0));
 }
 
 // Set position in a file
 off_t lseek(int file, off_t offset, int whence) {
-  return (off_t) sys_seek(file, (int) offset, whence);
+  return (off_t) set_errno(sys_seek(file, (int) offset, whence));
 }
 
 // Open a file
 int open(const char *name, int flags, ...) {
-  return sys_open(name, flags);
+  return set_errno(sys_open(name, flags));
 }
 
 // Read from a file
 int read(int file, void *ptr, size_t len) {
-  return sys_read(file, ptr, (uint) len);
+  return set_errno(sys_read(file, ptr, (uint) len));
 }
 
 // Increase program data space
 void* sbrk(ptrdiff_t incr) {
-  return sys_sbrk((int) incr);
+  int r = sys_sbrk((int) incr);
+  if(r < 0) {
+    errno = ENOMEM;
+    return (void*) -1;
+  } else {
+    return (void*) r;
+  }
 }
 
 
@@ -154,7 +170,7 @@ int stat(const char *file, struct stat *st) {
   fs_stat fs_st = {0};
   int res = sys_getattr_path(file, &fs_st);
   if(res < 0) {
-    return res;
+    return set_errno(res);
   }
   fs_stat2stat(&fs_st, st);
   return 0;
@@ -167,17 +183,17 @@ clock_t times(struct tms *buf) {
 
 // Remove a file’s directory entry
 int unlink(const char *name) {
-  return sys_unlink(name);
+  return set_errno(sys_unlink(name));
 }
 
 // Wait for a child process
 int wait(int *status) {
-  return sys_wait(status);
+  return set_errno(sys_wait(status));
 }
 
 // Write to a file. libc subroutines will use this system routine for output to all files, including stdout
 int write(int file, const void *ptr, size_t len) {
-  return sys_write(file, ptr, (uint) len);
+  return set_errno(sys_write(file, ptr, (uint) len));
 }
 
 // Get current clock time
@@ -186,7 +202,7 @@ int gettimeofday(struct timeval* tp, void* tz)
   date_time dt = {0};
   int res = sys_curr_date_time(&dt);
   if(res < 0) {
-    return res;
+    return set_errno(res);
   }
   tp->tv_sec = mktime((struct tm*) &dt);
   tp->tv_usec = 0;
@@ -195,13 +211,14 @@ int gettimeofday(struct timeval* tp, void* tz)
 
 int chdir(const char* path)
 {
-  return sys_chdir(path);
+  return set_errno(sys_chdir(path));
 }
 
 char* getcwd(char* buf, size_t size)
 {
   int r = sys_getcwd(buf, size);
   if(r < 0) {
+    errno = -r;
     return NULL;
   } else {
     return buf;
@@ -210,10 +227,10 @@ char* getcwd(char* buf, size_t size)
 
 int ftruncate (int fd, off_t length)
 {
-  return sys_truncate_fd(fd, (uint) length);
+  return set_errno(sys_truncate_fd(fd, (uint) length));
 }
 
 int truncate (const char * path, off_t length)
 {
-  return sys_truncate_path(path, (uint) length);
+  return set_errno(sys_truncate_path(path, (uint) length));
 }
